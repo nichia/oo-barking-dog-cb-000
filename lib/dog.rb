16fddae145fe7ffada3badfdf099837a834cb9a4
Ni Chia <nichia@gmail.com>
2017-03-16 03:31:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.name
+fido.bark
